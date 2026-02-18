0d67a6cc00cb50579280266eba1f54a7a9668f97
Manikanta <mailavarapumanikanta@gmail.com>
2026-02-18 08:14:05 +0530

    file updated

--- Shell-Script/datatypes.sh
+++ Shell-Script/datatypes.sh
@@ -8,7 +8,7 @@ SUM=$(($NUMBER1+$NUMBER2+$NAME))
 
 echo "sum is: $SUM"
 
-LEADERS={"MANI","SIVA"}
+LEADERS=("MANI" "SIVA")
 
 echo "all leaders are: ${LEADERS[@]}"
 echo "first leader is: ${LEADERS[0]}"

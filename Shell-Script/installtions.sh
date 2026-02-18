1a109ed2124eab01882ab7cd0b2f5830fb3a4046
Manikanta <mailavarapumanikanta@gmail.com>
2026-02-18 20:08:08 +0530

    file added

--- Shell-Script/installtions.sh
+++ Shell-Script/installtions.sh
@@ -2,13 +2,13 @@
 
 USERID =$(id -u);
 
-if [ USERID -ne 0 ]; then
+if [ $USERID -ne 0 ]; then
 echo "ERROR: please run the script with user"
 exit 1
 fi
 
 dnf install mysql -y
-if [ $1 -eq 0 ]; then
+if [ $? -eq 0 ]; then
 echo "mysql installation success"
 else
 echo "mysql installation failed"

8ecfb386c82ad7ffe4fdc07ffd6c6331340c63a2
Manikanta <mailavarapumanikanta@gmail.com>
2026-02-18 19:42:22 +0530

    file added

--- Shell-Script/Conditions1.sh
+++ Shell-Script/Conditions1.sh
@@ -4,6 +4,8 @@ NUMBER=$1
 
 if [ $NUMBER -lt 10 ]; then
 echo "number is less 10"
+elif [ $NUMBER -eq 10 ]; then
+echo "number is equal to 10"
 else
-echo "number is greater or equal to 10"
+echo "number is greater than to 10"
 fi

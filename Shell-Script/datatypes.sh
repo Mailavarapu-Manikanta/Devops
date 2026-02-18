70d3a092b7f423120afad121f94704acd475e8e9
Manikanta <mailavarapumanikanta@gmail.com>
2026-02-18 08:11:34 +0530

    file added

---
+++ Shell-Script/datatypes.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+NUMBER1=100
+NUMBER2=200
+NAME=DEVOPS
+
+SUM=$(($NUMBER1+$NUMBER2+$NAME))
+
+echo "sum is: $SUM"
+
+LEADERS={"MANI","SIVA"}
+
+echo "all leaders are: $LEADERS[@]"
+echo "first leader is: $LEADERS[0]"

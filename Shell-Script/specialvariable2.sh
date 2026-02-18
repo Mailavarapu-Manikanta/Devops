eb9c5115915e8c19e48baf35ae4b0aaa16aef61f
Manikanta <mailavarapumanikanta@gmail.com>
2026-02-18 08:07:34 +0530

    updated file2

--- Shell-Script/specialvariable2.sh
+++ Shell-Script/specialvariable2.sh
@@ -3,3 +3,6 @@ echo "file name:$0"
 echo "current working directory :$PWD"
 echo "who is running :$USER"
 echo "home directory $HOME"
+echo "process id:$$"
+sleep 50 &
+echo "pid of the last command in background $!"

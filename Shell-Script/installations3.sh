3b74112520126e79c38e749da7a5293d23f12a45
Manikanta <mailavarapumanikanta@gmail.com>
2026-02-18 22:24:41 +0530

    file added

--- Shell-Script/installations3.sh
+++ Shell-Script/installations3.sh
@@ -2,6 +2,9 @@
  
 USERID=$(id -u)
 
+R="\[e31m"
+G="\[e32m"
+N="\[e0m"
 if [ $USERID -ne 0 ]; then
 echo "ERROR : please use root user to run the script"
 fi
@@ -9,9 +12,9 @@ fi
 VALIDATE(){
 
 if [ $1 -eq 0 ];then
-echo "Installing $2 software is success"
+echo -e "$G Installing $2 software is success $N"
 else
-echo "Installing $1 software is failure"
+echo -e "$RInstalling $1 software is failure $N"
 exit 1
 fi
 
